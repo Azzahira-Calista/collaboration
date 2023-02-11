f265b56f196eca26e95158ab8e0babc2acaf0e1d
Tsabitah07 <122654070+Tsabitah07@users.noreply.github.com>
2023-02-11 08:32:49 +0700

    style:[IT] edit layout

--- app/src/main/java/com/example/collaboration/inas.java
+++ app/src/main/java/com/example/collaboration/inas.java
@@ -2,13 +2,30 @@ package com.example.collaboration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class inas extends AppCompatActivity {
 
+    Button btnBack;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inas);
+
+        btnBack = (Button) findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(inas.this,MainActivity.class);
+                startActivity(back);
+            }
+        });
     }
 }
